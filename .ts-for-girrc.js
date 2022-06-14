5a964aadf96d07401adf464c51a947e48464ad7a
d10221 <d10221@protonmail.com>
2022-06-14 19:29:34 +0200

    format

--- .ts-for-girrc.js
+++ .ts-for-girrc.js
@@ -1,7 +1,3 @@
 export default {
-    "ignore": [
-        "Clutter-1.0",
-        "CoglPango-1.0",
-        "Cogl-1.0"
-    ]
-}
+  ignore: ["Clutter-1.0", "CoglPango-1.0", "Cogl-1.0"],
+};
--- app/main.tsx
+++ app/main.tsx
@@ -1,22 +1,22 @@
-import Gtk from "../@types/Gjs/Gtk-3.0";
+import Gtk, { Button, Window } from "../@types/Gjs/Gtk-3.0";
 import v from "../lib/gjsx";
 
 Gtk.init(null);
 
 let i = 0;
-function clicked(sender: any) {
+function clicked(sender: Button) {
   sender.label = `Clicks=${++i}`;
 }
 //
-const widget = (
-  <window     
+const widget: Window = (
+  <window
     title="Hellow World"
     border_width={100}
-    signals={{ "destroy": () => Gtk.main_quit() }}
+    signals={{ destroy: () => Gtk.main_quit() }}
   >
     <box>
-      <button label="Click me!" signals={{ clicked: clicked }} />
-      <button label="Click me!" signals={{ clicked, }} />
+      <button label="Click me!" signals={{ clicked }} />
+      <button label="Click me!" signals={{ clicked }} />
     </box>
   </window>
 );
