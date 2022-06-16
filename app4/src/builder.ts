0cdc3f9ca378a14263221ecaf423ba4f3662d34c
d10221 <d10221@protonmail.com>
2022-06-16 07:38:38 +0200

    ... clean

--- app4/src/builder.ts
+++ app4/src/builder.ts
@@ -1,6 +1,5 @@
 import { Builder } from "@local/gjs/types/gjs/Gtk-3.0";
 
 export default function build(string: string) {
-  log(string)
   return Builder.new_from_string(string, string?.length);
 }
