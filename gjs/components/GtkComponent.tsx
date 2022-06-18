64f03bd5ee2521da6658e41549b5e69d3986f050
d10221 <d10221@protonmail.com>
2022-06-18 20:32:37 +0200

    GtkPacking

--- gjs/components/GtkComponent.tsx
+++ gjs/components/GtkComponent.tsx
@@ -1,11 +1,15 @@
 import { jsxml } from "@local/jsxml";
 type GtkProps<T> = T & { children?: any; id?: string };
 /**
- *
+ * Component Fty
  * @param className
- * @returns GtkElement
+ * @returns Component
  */
 export default function GtkComponent<T>(className: JSX.GtkClass) {
+  /**
+   * Component
+   * @returns element
+   */
   return ({ children, id, ...props }: GtkProps<T>) => (
     <object class={className} id={id}>
       {Object.entries(props).map(([key, value]) => (
@@ -24,7 +28,9 @@ function propValue(value: any) {
   if (typeof value === "boolean") return value ? "True" : "False";
   return value;
 }
-
+/**
+ * 
+ */
 type PackingProps = {
   expand?: boolean;
   fill?: boolean;
@@ -33,18 +39,23 @@ type PackingProps = {
   "pack-type"?: "end" | "start";
   children?: any;
 };
-
+/**
+ * 
+ * @param props 
+ * @returns 
+ */
 export function GtkPacking({ children, ...props }: PackingProps) {
-  const packingProps = Object.entries(props).map(([key, value]) => (
-    <property name={key}>{propValue(value)}</property>
-  ));
+  const packingProps = () =>
+    Object.entries(props).map(([key, value]) => (
+      <property name={key}>{propValue(value)}</property>
+    ));
   if (children) {
     return (
       <child>
         {children}
-        <packing>{packingProps}</packing>;
+        <packing>{packingProps()}</packing>;
       </child>
     );
   }
-  return <packing>{packingProps}</packing>;
+  return <packing>{packingProps()}</packing>;
 }
