f1e17a8c4779bad7a14896f524f4ed8a48b850b3
Mr. D <danieliozzi@gmail.com>
2022-06-22 15:51:52 +0200

    gjs doesn't like promises? setTimeout ?

--- app5/src/main.tsx
+++ app5/src/main.tsx
@@ -1,22 +1,29 @@
 #!/usr/bin/env gjs
-import { main, main_quit, init, Application } from "@local/gjs";
-import mainWindow from "./ui/main-window";
-import store from "./store";
-
-init(null);
-
-const app = new Application({
-  application_id: "org.local.app4",
-});
-app.connect("activate", (app: Application) => {
-  let win = app.active_window;
-  if (!win) {
-    win = mainWindow(store);
-    win.connect("delete-event", () => {
-      main_quit(); //exit app
-    });
-  }
-  win.present();
-});
-app.run(null);
-main(); // mainloop start until quit
+// @ts-ignore
+imports.gi.versions.Gtk = "3.0";
+const { Gtk } = imports.gi;
+/** */
+(async () => {  
+  Gtk.init(null);
+  const { default: store } = await import("./store");
+  const { default: mainWindow } = await import("./ui/main-window");
+  const pkg = await import("../package.json");
+  log(`pkg.id=${pkg.id}`);
+  //
+  const app = new Gtk.Application({
+    application_id: pkg.id,
+    flags: imports.gi.Gio.ApplicationFlags.FLAGS_NONE,
+  });
+  app.connect("activate", (app) => {
+    let win = app.active_window;
+    if (!win) {
+      win = mainWindow(store);      
+      win.connect("delete-event", () => {        
+        Gtk.main_quit(); //exit app
+      });
+    }
+    win.present();
+  });
+  app.run(null);
+  Gtk.main(); // mainloop start until quit
+})();
--- app5/src/ui/main-window/connect.ts
+++ app5/src/ui/main-window/connect.ts
@@ -15,24 +15,20 @@ export default (builder: Gtk.Builder) => (store: Store) => {
   const label1 = getObject<Gtk.Label>(builder, "_label1");
   const menuModel = getObject<Gio.MenuModel>(menu, "menu");
 
+  async function quitAsync() {
+    log("Quiting");
+    button.label = "Goodbye!";
+    // await new Promise(resolve => setTimeout(resolve, 100)); // It doesn't work
+    log("Closing")
+    window.close();
+  }
   // State change subscription
   store.subscribe(() => {
     const { title, label, clicks, quit } = store.getState();
     window.title = title;
     label1.label = label;
     button.label = clicks == 0 ? "Click me!" : `clicks=${clicks}`;
-    if (quit) {
-      import("./dialog")
-        .then((x) => build(x.default))
-        .then((x) => x.get_object("dialog1") as Gtk.MessageDialog)
-        .then((dialog) => {
-          dialog.text = "Quit?";
-          // TODO: show dialog, get result, and quit
-          const x = dialog.show_all();
-          window.close();
-        })
-        .catch(console.error);
-    }
+    if (quit) quitAsync()
   });
   // connect dispatch
   button.connect("clicked", () => store.dispatch({ type: "click" }));
@@ -46,3 +42,17 @@ export default (builder: Gtk.Builder) => (store: Store) => {
   menuButton.set_menu_model(menuModel);
   return window;
 };
+/**
+ * TODO
+ */
+function dialog() {
+  import("./dialog")
+    .then((x) => build(x.default))
+    .then((x) => x.get_object("dialog1") as Gtk.MessageDialog)
+    .then((dialog) => {
+      dialog.text = "Quit?";
+      // TODO: show dialog, get result, and quit
+      const x = dialog.show_all();
+    })
+    .catch(console.error);
+}
