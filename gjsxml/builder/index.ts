986cbf3dba91d1b682be2e76fd2ac984f4397604
Mr. D <danieliozzi@gmail.com>
2022-06-22 11:52:31 +0200

    problems with wsl & gjs 1.64.5

--- gjsxml/builder/index.ts
+++ gjsxml/builder/index.ts
@@ -1,17 +1,17 @@
-import { Builder } from "@local/gjs/";
+import type  * as Gtk from "@local/gjs/";
 import type * as GObject from "@local/gjs/Gjs/GObject-2.0";
 /**
  *
  * @param string
  * @returns
  */
-export function build(string: string) {
-  return Builder.new_from_string(string, string?.length);
+export function build(string: string) {  
+  return imports.gi.Gtk.Builder.new_from_string(string, string?.length);
 }
 /**
  * nah?
  */
-export function getObjectFty(builder: Builder) {
+export function getObjectFty(builder: Gtk.Builder) {
   return <T extends GObject.Object = GObject.Object>(name: string) =>
     builder.get_object(name) as T;
 }
@@ -19,7 +19,7 @@ export function getObjectFty(builder: Builder) {
  * nah?
  */
 export function getObject<T extends GObject.Object = GObject.Object>(
-  builder: Builder,
+  builder: Gtk.Builder,
   name: string
 ) {
   return builder.get_object(name) as T;
